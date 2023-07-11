c5b0845d6c819cc52b8fec492e29ef27e5026942
InSeoLee4749 <in474900@gmail.com>
2023-07-11 17:57:14 +0900

    contact 배경img 설정

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -5,7 +5,7 @@ export default function Contact(props) {
     return (
         <>
         <section  id={props.idnm}>
-            <div className={`container d-flex flo`}>
+            <div className={`flo container d-flex mt-5`}>
                 <div>
                     <p>함께 걸으면서 지구를 지켜볼래?</p>
                     <p>The Body Shop과 함께하는 "플로깅 첼린지"</p>
--- src/component/Review.js
+++ src/component/Review.js
@@ -5,54 +5,57 @@ export default function Review(props) {
     return (
         <>
             <section  id={props.idnm}>
-                <div>
-                    <h3 className="text-center">BEST REVIEW</h3>
-                    <p className="text-center mt-1">직접 써보고 말하는 생생한 리얼 후기!</p>
+                <div className="mt-5">
+                    <div>
+                        <h3 className="text-center">BEST REVIEW</h3>
+                        <p className="text-center mt-1">직접 써보고 말하는 생생한 리얼 후기!</p>
+                    </div>
+                    <div className={`container d-flex mt-2 ${review.box}`}>
+                        <div>
+                            <img src="./img/product1.PNG" alt=""/>
+                            <p>루시어스 리치 샤워 크림</p>
+                            <p>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-half"></i>
+                            4.5</p>
+                            <div>
+                                리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./img/product1.PNG" alt=""/>
+                            <p>루시어스 리치 샤워 크림</p>
+                            <p>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-half"></i>
+                            4.5</p>
+                            <div>
+                                리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./img/product1.PNG" alt=""/>
+                            <p>루시어스 리치 샤워 크림</p>
+                            <p>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-half"></i>
+                            4.5</p>
+                            <div>
+                                리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className={`container d-flex mt-2 ${review.box}`}>
-                    <div>
-                        <img src="./img/product1.PNG" alt=""/>
-                        <p>루시어스 리치 샤워 크림</p>
-                        <p>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-half"></i>
-                        4.5</p>
-                        <div>
-                            리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
-                        </div>
-                    </div>
-                    <div>
-                        <img src="./img/product1.PNG" alt=""/>
-                        <p>루시어스 리치 샤워 크림</p>
-                        <p>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-half"></i>
-                        4.5</p>
-                        <div>
-                            리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
-                        </div>
-                    </div>
-                    <div>
-                        <img src="./img/product1.PNG" alt=""/>
-                        <p>루시어스 리치 샤워 크림</p>
-                        <p>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-fill"></i>
-                            <i className="bi bi-star-half"></i>
-                        4.5</p>
-                        <div>
-                            리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
-                        </div>
-                    </div>
-                </div>
+
             </section>
         </>
     );
