9bc9b4a47f5fad98792046bd3e55d3d02d6dabab
Joohong-kim <zzangtra@gmail.com>
2023-07-13 16:20:14 +0900

    logo & gnb 수정

--- src/layout/Hd.js
+++ src/layout/Hd.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import hd from "../scss/hd.module.css";
 import { Navbar, Nav } from 'react-bootstrap';
 import Scrollspy from 'react-scrollspy';
@@ -7,17 +7,38 @@ import Scrollspy from 'react-scrollspy';
 
 export default function Hd(props) {
 
+    const [scrollPosition, setScrollPosition] = useState(0);
+ 
+    
+
+    const updateScroll = () => {
+        setScrollPosition( window.pageYOffset );      
+               
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+       
+        
+        return(
+            ()=>{
+                window.removeEventListener('scroll', updateScroll)
+            }
+        )
+    }, []);
+    // {scrollPosition > 100 && "scrolldown" }
+    // scrollPosition < 100 ? "original_header" : "change_header" `${hd.navbar}`
     return (
-    <header>
-    <Navbar className="fixed-top d-block" expand="lg">
-        <Navbar.Brand as="h1"><a href="#top"><img src="./img/svg/The_Body_Shop_logo.svg"></img></a></Navbar.Brand>
+    <header  className={ scrollPosition > 100 && "scrolldown " } id="hd">
+    <Navbar className={`${hd.navbar} flex-wrap`}  expand="lg" >
+      
+        <Navbar.Brand as="h1" className="col-lg-12 d-flex justify-content-center overflow-hidden "><a href="#top" className="d-block"><img src="./img/svg/The_Body_Shop_logo.svg"></img></a></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="">
             <Scrollspy  currentClassName="active">
                 {
                 props.dbsrc.gnb.map( (v, x) =>{
-                    return  <li><Nav.Link href={"#"+v.href}>{v.title}</Nav.Link></li>
+                    return  <li key={x}><Nav.Link href={"#"+v.href}>{v.title}</Nav.Link></li>
                 })  
             
                 }
