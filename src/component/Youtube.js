b1283a17b276bc58c485ddc23998339c668481be
InSeoLee4749 <in474900@gmail.com>
2023-07-17 17:27:30 +0900

    gnb 색상수정 & 유튜브 삽입

--- src/component/Youtube.js
+++ src/component/Youtube.js
@@ -4,9 +4,11 @@ import youtube from "../scss/youtube.module.css";
 export default function Youtube(props) {
     return (
         <>
-            <div className={`container bg-light mt-5 ${youtube.height}`}>
-                유튜브 컨텐츠입니다.
+            <div className={`container mt-5 ${youtube.height}`}>
+                <iframe className={`${youtube.youtube}`} width="560" height="315" src="https://www.youtube.com/embed/cPu69x2cHT8" title="YouTube video player" 
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
+            
         </>
     );
 }
