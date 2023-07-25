3d4f1a3f48918b2da96c7973aa33dc030b6d1315
InSeoLee4749 <in474900@gmail.com>
2023-07-25 15:33:59 +0900

    pick 수정

--- src/component/Pick.js
+++ src/component/Pick.js
@@ -1,62 +1,155 @@
 import React from "react";
-import pick from "../scss/pick.module.css";
-import { Swiper, SwiperSlide } from "swiper/react";
+import review from "../scss/pick.module.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from "swiper";
 
-export default function Pick(props) {
+export default function Review(props) {
     return (
         <>
-            <section id={props.idnm} className={`${pick.section}`}>
+            {/* <section  id={props.idnm}>
                 <div className="mt-5">
                     <div>
-                        <h3 className={`text-center ${pick.title}`}>BEST PICK</h3>
+                        <h3 className={`text-center ${review.title}`}>BEST REVIEW</h3>
+                        <p className="text-center mt-1">직접 써보고 말하는 생생한 리얼 후기!</p>
                     </div>
-                    <div className={`container p-0 mt-3`}>
-                        <Swiper
-                            spaceBetween={30}
-                            centeredSlides={true}
-                            slidesPerView={4}
-                            pagination={{
-                                clickable: true,
-                            }}
-                            loop={true}
-                            navigation={true}
-                            modules={[Pagination, Navigation]}
-                            className="mySwiper"
-                        >
-                                <SwiperSlide className={`${pick.sw1}`}>
-                                    <div>
-                                        <img src="./img/pick-1.jpg" alt=""/>
-                                        <div className="text-center">
-                                            <p>루시어스 리치 샤워 크림</p>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className={`${pick.sw2}`}>
-                                    <div>
-                                        <img src="./img/pick-1.jpg" alt=""/>
-                                        <p>루시어스 리치 샤워 크림</p>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className={`${pick.sw3}`}>
-                                    <div>
-                                        <img src="./img/pick-1.jpg" alt=""/>
-                                        <p>루시어스 리치 샤워 크림</p>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className={`${pick.sw4}`}>
-                                    <div>
-                                        <img src="./img/pick-1.jpg" alt=""/>
-                                        <p>루시어스 리치 샤워 크림</p>
-                                    </div>
-                                </SwiperSlide>
-                                <SwiperSlide className={`${pick.sw5}`}>slide5</SwiperSlide>
-                                <SwiperSlide className={`${pick.sw6}`}>slide6</SwiperSlide>
-                        </Swiper>
+                    <div className={`container d-flex mt-2 ${review.box}`}>
+                        <div>
+                            <img src="./img/review-1.jpg" alt=""/>
+                            <p>루시어스 리치 샤워 크림</p>
+                            <p>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-half"></i>
+                            4.5</p>
+                            <div>
+                                리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./img/review-2.jpg" alt=""/>
+                            <p>루시어스 리치 샤워 크림</p>
+                            <p>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-half"></i>
+                            4.5</p>
+                            <div>
+                                리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
+                            </div>
+                        </div>
+                        <div>
+                            <img src="./img/review-3.jpg" alt=""/>
+                            <p>루시어스 리치 샤워 크림</p>
+                            <p>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-fill"></i>
+                                <i className="bi bi-star-half"></i>
+                            4.5</p>
+                            <div>
+                                리치향이 강하지 않고 은은해요!가격대비 용량도 좋고, 적은 양으로 거품도 많이 나와요~
+                            </div>
+                        </div>
                     </div>
                 </div>
-                
-            </section>
+
+            </section> */}
+            <div className={`${review.content}`}>
+                {/* <div className={`${review.title}`}>
+                    Best Review
+                </div> */}
+                <Swiper
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    slidesPerView={3}
+                    // pagination={{
+                    //         type: 'progressbar',
+                    //         }}
+                    loop={true}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className={`${review.mySwiper} container`}
+                    >
+                    <SwiperSlide className={`${review.swiperslide}`}>
+                        <div className={`${review.info_top}`}>
+                            <p className={`${review.pplen}`}>저자극 비건 바디케어</p>
+                            <p className={`${review.emname}`}>SHOWER GEL MORINGA</p>
+                            <p className={`${review.krname}`}>모링가 대용량 샤워젤</p>
+                        </div>
+                        <div className={`${review.imgdiv}`}>
+                            <a href=""><img className={`${review.img}`} src="./img/1.png" alt="" /></a>
+                        </div>
+                        <div className={`${review.info_box} row`}>
+                            <span>750ml</span>
+                            <span>31,000</span>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className={`${review.swiperslide}`}>
+                    <div className={`${review.info_top}`}>
+                            <p className={`${review.pplen}`}>촉촉한 미스트</p>
+                            <p className={`${review.emname}`}>BODY MIST ZESTY LIME BL</p>
+                            <p className={`${review.krname}`}>제스티 라임 블라썸 바디미스트</p>
+                        </div>
+                        <div className={`${review.imgdiv}`}>
+                            <a href=""><img className={`${review.img}`} src="./img/2.png" alt="" /></a>
+                        </div>
+                        <div className={`${review.info_box} row`}>
+                            <span>100ml</span>
+                            <span>21,000</span>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className={`${review.swiperslide}`}>
+                        <div className={`${review.info_top}`}>
+                            <p className={`${review.pplen}`}>저자극 비건 선크림</p>
+                            <p className={`${review.emname}`}>Vita Tone Up Sun Cream</p>
+                            <p className={`${review.krname}`}>비타 톤업 선크림</p>
+                        </div>
+                        <div className={`${review.imgdiv}`}>
+                            <a href=""><img className={`${review.img}`} src="./img/3.png" alt="" /></a>
+                        </div>
+                        <div className={`${review.info_box} row`}>
+                            <span>50ml</span>
+                            <span>21,000</span>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className={`${review.swiperslide}`}>
+                        <div className={`${review.info_top}`}>
+                            <p className={`${review.pplen}`}>저자극 비건 바디케어</p>
+                            <p className={`${review.emname}`}>SHOWER GEL MORINGA</p>
+                            <p className={`${review.krname}`}>모링가 대용량 샤워젤</p>
+                        </div>
+                        <div className={`${review.imgdiv}`}>
+                            <a href=""><img className={`${review.img}`} src="./img/4.png" alt="" /></a>
+                        </div>
+                        <div className={`${review.info_box} row`}>
+                            <span>750ml</span>
+                            <span>31,000</span>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide className={`${review.swiperslide}`}>
+                    <div className={`${review.info_top}`}>
+                            <p className={`${review.pplen}`}>저자극 비건 선크림</p>
+                            <p className={`${review.emname}`}>Vita Tone Up Sun Cream</p>
+                            <p className={`${review.krname}`}>비타 톤업 선크림</p>
+                        </div>
+                        <div className={`${review.imgdiv}`}>
+                            <a href=""><img className={`${review.img}`} src="./img/5.png" alt="" /></a>
+                        </div>
+                        <div className={`${review.info_box} row`}>
+                            <span>50ml</span>
+                            <span>21,000</span>
+                        </div>
+                    </SwiperSlide>
+
+                </Swiper>
+                <div></div>
+            </div>
         </>
+        
     );
 }
--- src/layout/Content.js
+++ src/layout/Content.js
@@ -5,7 +5,6 @@ import Solution from "../component/Solution";
 import Pick from "../component/Pick";
 import Youtube from "../component/Youtube";
 import News from "../component/News";
-import Review from "../component/Review";
 import Contact from "../component/Contact";
 
 export default function Content(props) {
@@ -14,9 +13,9 @@ export default function Content(props) {
         <>
             <Banner></Banner>
             <Service></Service>
-            <Review  idnm={props.dbsrc.gnb[2].href}></Review>
+            <Pick  idnm={props.dbsrc.gnb[2].href}></Pick>
             {/* <Solution></Solution> */}
-            <Pick  idnm={props.dbsrc.gnb[0].href}></Pick>
+            {/* <Pick  idnm={props.dbsrc.gnb[0].href}></Pick> */}
             {/* <Youtube></Youtube> */}
             {/* <News  idnm={props.dbsrc.gnb[1].href}></News> */}
             {/* <Contact  idnm={props.dbsrc.gnb[3].href}></Contact> */}
